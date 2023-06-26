a9364de85c470823adb505af4e08fa832de8a80a
Harman <harmanjyot.singh@chicmic.co.in>
2023-06-26 10:58:09 +0530

    CR: created new users class.

---
+++ MusicPlayer-User/src/main/java/com/example/MusicPlayer/model/Users.java
@@ -0,0 +1,28 @@
+package com.example.MusicPlayer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@Getter@Setter
+@RequiredArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private Date dob;
+    private boolean verified;
+    private String uuid;
+}
